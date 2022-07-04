973c9bd0d18ebf8d94a0c140bc9a997983e197a8
MaksimNikitashin <100536331+MaksimNikitashin@users.noreply.github.com>
2022-07-04 20:58:22 +0200

    Add files via upload

--- 1.task/contactMe.js
+++ 1.task/contactMe.js
@@ -4,16 +4,16 @@ function sendEmail(){
     let phone = document.getElementById("phone").value;
     let message = document.getElementById("message").value;
 
-    if(name != "" && surname != "" && phone != "" && message != ""){
-        document.getElementById("errorMessage").style.visibility = "hidden";
+    /*if(name != "" && surname != "" && phone != "" && message != ""){
+        document.getElementById("errorMessage").style.visibility = "hidden";*/
         window.open('mailto:nikitashin_m@mail.ru?subject=' + 'Contact_me_via_my_web-page' + '&body=' + 
         'Name: ' + name + '%0D%0A' +
         'Surname: ' + surname + '%0D%0A' +
         'Phone: ' + phone + '%0D%0A' +
         'Message: ' + message);
-    }
+    //}
 
-    if(name == "" || surname == "" || phone == "" || message == ""){
+    /*if(name == "" || surname == "" || phone == "" || message == ""){
         document.getElementById("errorMessage").style.visibility = "visible";
-    }
+    }*/
 }
